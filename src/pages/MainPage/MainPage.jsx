b29e494c1cb62055b59c5c6293831cc1893f59a5
steven.yeo <steven.y@dynrtech.com>
2022-12-10 19:59:32 +0700

    20221210 - Initiate folder structure

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = (props) => {
+    return (
+        <h1 style={{ color: '#BF1922'}}>
+            Matahari Main Page
+        </h1>
+    )
+};
+
+export default MainPage;
